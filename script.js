10cbdb1cf06cb384b31c32d02f4c8963f42f12b8
moskin-cpu <markoskin1@gmail.com>
2025-11-24 18:09:23 -0600

    Add alert for questions button on click

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+// Questions button alert
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("questionBtn");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("If you have questions, contact me at:\nmoskin@cps.edu");
+    });
+  }
+});
